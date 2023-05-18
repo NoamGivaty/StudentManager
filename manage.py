207bf565a0b955608a1b6c1100438a6e3a2f313b
Noam Givaty <noamgivaty@gmail.com>
2023-05-18 09:13:02 +0300

    Checking swagger and GET

--- manage.py
+++ manage.py
@@ -3,7 +3,14 @@ from flask_script import Manager
 import unittest
 from app import blueprint
 from app.main import create_app
+from flask_migrate import Migrate, MigrateCommand
+from app.main import db
+from app.main.model import student
+from flask import Flask
+from flask.cli import FlaskGroup
 
+
+app = Flask(__name__)
 
 app = create_app()
 app.register_blueprint(blueprint)
@@ -11,11 +18,18 @@ app.register_blueprint(blueprint)
 app.app_context().push()
 manager = Manager(app)
 
+cli = FlaskGroup(app)
+
+if __name__ == '__main__':
+    cli()
+
 
 @manager.command
 def run():
     app.run()
 
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
@@ -28,3 +42,4 @@ def test():
 
 if __name__ == '__main__':
     manager.run()
+
